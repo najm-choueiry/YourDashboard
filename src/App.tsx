dda84a7d5e017fef5515346618884c396f42f3b9
najm-choueiry <nachoueiry@outlook.com>
2024-03-22 15:21:03 +0200

    <> Modifying the auth login

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,9 @@ function App() {
                 <Routes>
                   <Route index element={<WelcomePage />} />
                   <Route index element={<Home/>}/>
+                  <Route path="/forgot-password" element={<ForgotPassword/>}/>
+                  <Route path="/register" element={<Register/>}/>
+                  <Route path="/login" element={<Login/>}/>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
