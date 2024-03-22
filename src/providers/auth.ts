aa22fc74a5169cd5e8389cfc9dc07b7f8fbcc099
najm-choueiry <nachoueiry@outlook.com>
2024-03-22 09:38:32 +0200

    <> Creating the auth file

---
+++ src/providers/auth.ts
@@ -0,0 +1,9 @@
+import { AuthBindings } from "@refinedev/core"
+
+import { API_URL, dataProvider } from "./data"
+
+export const authCredentials = {
+    email: "admin@gmail.com",
+    password: "admin123"
+}
+
