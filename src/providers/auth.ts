c359625b4e13cc8ce8a15ef1a50966ce1392bff2
najm-choueiry <nachoueiry@outlook.com>
2024-03-22 10:03:30 +0200

    <> Creating the login mutation

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -7,3 +7,43 @@ export const authCredentials = {
     password: "admin123"
 }
 
+export const authProvider: AuthBindings = {
+    login: async({email}) => {
+        try {
+            const {data} = await dataProvider.custom({
+                url: API_URL,
+                method: "post",
+                headers: {},
+                meta: {
+                    variables: {email},
+                    rawQuery: `
+                    mutation Login($email: String!){
+                        login(loginInput: {email: $email}) {
+                            accessToken
+                        }
+                    }
+                    `
+                }
+            });
+
+            localStorage.setItem("access_token", data.login.accessToken)
+
+            return {
+                success: true,
+                redirectTo: "/"
+            };
+        } catch (e) {
+            const error = e as Error;
+        
+            return {
+                success: false,
+                error: {
+                    message: "message" in error? error.message : "Login Failed",
+                    name: "name" in error ?  error.name : "Invalid email or password"
+                    
+                }
+            }
+        
+        }
+    }
+}
