b8e14796f7f186005309cc2b9e98f141150e5973
najm-choueiry <nachoueiry@outlook.com>
2024-03-22 19:01:53 +0200

    <> Updating the custom avatar with the User info

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,20 +1,22 @@
 import {Avatar as AntdAvatar, AvatarProps} from 'antd'
 
 type Props = AvatarProps & {
-    name: string
+    name?: string
 }
 
 const CustomAvatar = ({name , style , ...rest}: Props ) => {
   return (
       <AntdAvatar
-        alt={"John Doe"}
+        alt={name}
         size="default"
         style={{
           backgroundColor: '#87d068',
           display: "flex",
           alignItems: "center",
-          border: "none"
+          border: "none",
+          ...style
         }}
+        {...rest}
       >
         {name}
       </AntdAvatar>
